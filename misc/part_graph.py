47471d8efbd70c4c5a28cf051b4f5814222de3e1
Vipin Jain <jan.vipin@gmail.com>
2012-05-09 08:32:28 -0400

    dom dump for neo4j poc

---
+++ misc/part_graph.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# part_file to test neo4j poc
+import sys
+import networkx as nx
+
+f = open(sys.argv[1].'r')
+g = open("part_graph_op.txt", 'w')
+gr = cPicle.load(f)
+f.close()
+ct = 0
+for n in gr.nodes():
+    if ct  == 1500000:
+        break
+    else:
+        ct += 1
+        for e in gr.edge[n]:
+            g.write("%s, %s, %s, %s, %s \n" %(n, e, gr.node[n]['type'], gr.node[e]['type'], gr.edge[n][e]['type']))
+            
+g.close()
+
